38639a9a48249db06b8a11bc6484d73f682caa89
danny-sg <danny.gould@gmail.com>
2023-12-23 17:39:44 +0000

    added Installer profile

---
+++ src/InternalsViewer.Tests.Internals.IntegrationTests/Test Data/TestDatabase/CreateDatabase.sql
@@ -0,0 +1,20 @@
+﻿-- Create a test database 
+
+DROP DATABASE IF EXISTS TestDatabase
+GO
+
+USE TestDatabase
+GO
+
+CREATE SCHEMA Testing
+GO
+
+CREATE TABLE Testing.Table_Clustered
+    (
+     Id                  INT IDENTITY(1, 1) PRIMARY KEY CLUSTERED
+    ,VarcharNotNullValue VARCHAR(100) NOT NULL
+    ,VarcharNullValue    VARCHAR(100) NULL
+    ,IntNotNullValue     INT          NULL
+    ,IntNullValue        INT          NULL
+    )
+GO
---
+++ src/InternalsViewer.Tests.Internals.IntegrationTests/TestHelpers/DatabaseDumper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternalsViewer.Tests.Internals.IntegrationTests.TestHelpers;
+
+public class DatabaseDumper
+{
+    public Task DumpDatabase(string databaseName, string outputFolder)
+    {
+        var sql = $@"DECLARE @DatabaseName sysname = '{databaseName}'"
+    }
+}
